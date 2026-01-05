19ff578cd9259626af617ce879be053834117638
yagud777 <yagud777@gmail.com>
2026-01-05 23:11:02 +0300

    t

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   transpilePackages: ['@world-boss/shared'],
   experimental: {
     optimizePackageImports: ['framer-motion'],
